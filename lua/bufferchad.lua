3e69f84b158875abbd68af9a1389a03f17b2a308
mrquantumcodes <mrquantumtech@gmail.com>
2023-09-25 15:13:05 +0530

    Added options to change sorting order

--- lua/bufferchad.lua
+++ lua/bufferchad.lua
@@ -4,41 +4,41 @@ local M = {}
 M.opts = {}
 
 M.setup = function(options)
-
 	M.opts = options
 
 	local keybinding = options.mapping or "<Leader>bb"
 
-	vim.api.nvim_set_keymap('n', keybinding, "", { noremap = true, silent = true, callback = function() M.BufferChadListBuffers() end })
+	vim.api.nvim_set_keymap('n', keybinding, "",
+		{ noremap = true, silent = true, callback = function() M.BufferChadListBuffers() end })
 end
 
 function removePathFromFullPath(fullPath, pathToRemove)
-    -- Replace backslashes with forward slashes for platform independence
-    fullPath = fullPath:gsub("\\", "/")
-    pathToRemove = pathToRemove:gsub("\\", "/")
+	-- Replace backslashes with forward slashes for platform independence
+	fullPath = fullPath:gsub("\\", "/")
+	pathToRemove = pathToRemove:gsub("\\", "/")
 
-    -- Normalize paths by removing trailing slashes
-    fullPath = fullPath:gsub("/$", "")
-    pathToRemove = pathToRemove:gsub("/$", "")
+	-- Normalize paths by removing trailing slashes
+	fullPath = fullPath:gsub("/$", "")
+	pathToRemove = pathToRemove:gsub("/$", "")
 
-    local fullPathLen = #fullPath
-    local pathToRemoveLen = #pathToRemove
+	local fullPathLen = #fullPath
+	local pathToRemoveLen = #pathToRemove
 
-    local i = 1
-    while i <= fullPathLen and i <= pathToRemoveLen do
-        if fullPath:sub(i, i) == pathToRemove:sub(i, i) then
-            i = i + 1
-        else
-            break
-        end
-    end
+	local i = 1
+	while i <= fullPathLen and i <= pathToRemoveLen do
+		if fullPath:sub(i, i) == pathToRemove:sub(i, i) then
+			i = i + 1
+		else
+			break
+		end
+	end
 
-    if i > pathToRemoveLen then
-        -- Remove pathToRemove and any leading slash
-        return fullPath:sub(i + 1)
-    else
-        return fullPath
-    end
+	if i > pathToRemoveLen then
+		-- Remove pathToRemove and any leading slash
+		return fullPath:sub(i + 1)
+	else
+		return fullPath
+	end
 end
 
 -- Function to open the buffer list window in order of usage with the first and second buffers swapped
@@ -50,17 +50,17 @@ M.BufferChadListBuffers = function()
 	local buffer_list
 
 	if sort_order == "LAST_USED_UP" or sort_order == "DESCENDING" then
-	  local buffer_list = vim.fn.execute("ls t")
-	else if sort_order == "REGULAR" then
+		local buffer_list = vim.fn.execute("ls t")
+	elseif sort_order == "REGULAR" then
 		local buffer_list = vim.fn.execute("ls")
 	end
-  
+
 	-- Split the buffer list into lines
 	local buf_names = vim.split(buffer_list, "\n")
-  
+
 	-- Remove the first line (header)
 	table.remove(buf_names, 1)
-  
+
 	-- Check if there are at least two buffers
 
 	if sort_order == "LAST_USED_UP" then
@@ -86,14 +86,14 @@ M.BufferChadListBuffers = function()
 	local path1 = cwdpath
 	local path2 = ""
 
-  
+
 	-- Extract the buffer names within double quotes
 	local buffer_names = {}
 	for _, line in ipairs(buf_names) do
 		local name = line:match('"([^"]+)"')
 		if name then
 			local myname = name:gsub("%~", vim.fn.expand('$HOME')):gsub("\\", "/")
-			
+
 			path2 = myname
 
 			-- print(path1, path2)
@@ -105,23 +105,22 @@ M.BufferChadListBuffers = function()
 			table.insert(buffer_names, remainingPath)
 		end
 	end
-  
+
 	vim.ui.select(buffer_names, {
-	  prompt = "Navigate to a Buffer",
+		prompt = "Navigate to a Buffer",
 	}, function(selected)
-	  if selected ~= "" and selected ~= nil and selected ~= '[No Name]' then
-		vim.cmd('buffer ' .. selected)
-	  end
+		if selected ~= "" and selected ~= nil and selected ~= '[No Name]' then
+			vim.cmd('buffer ' .. selected)
+		end
 	end)
-end
 end
 
 
 -- vim.cmd([[command! BufferChadListBuffers lua BufferChadListBuffers() ]])
 
-  
 
-  -- Set the keybinding to toggle the buffer list window
+
+-- Set the keybinding to toggle the buffer list window
 --   vim.api.nvim_set_keymap('n', '<leader>bb', '<Cmd>lua OpenBufferListWindow()<CR>', { noremap = true, silent = true })
 
 return M
